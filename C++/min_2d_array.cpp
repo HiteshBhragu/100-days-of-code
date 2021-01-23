c4822773924386650ad148c7a87d5f96bfbb6013
Hitesh Bhragu <hitesh.bhragu.sdbc@gmail.com>
2021-01-23 23:19:57 +0530

    C++

---
+++ C++/min_2d_array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+#define ROWS 3
+#define COLS 3
+{
+    int matrix[ROWS][COLS]={ {1,2,3}, {1,2,3}, {1,2,3} };
+
+    int matrix2[ROWS][COLS];
+    int row,col,max=matrix[0][0];
+
+    for(row=0;row<ROWS;row++)
+    {
+        for(col=0;col<COLS;col++)
+        {
+            if(max>matrix[row][col])
+            {
+                max = matrix[row][col];
+            }
+        }
+    }
+    cout<<max;
+}
