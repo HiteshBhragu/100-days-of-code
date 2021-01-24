1f5fa8999adebdacd2566828f8605ee72325ae5c
Hitesh Bhragu <hitesh.bhragu.sdbc@gmail.com>
2021-01-24 21:59:05 +0530

    C++

---
+++ C++/add element in array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[50];
+    int i,n;
+
+    for(i=0;i<6;i++)
+    {
+        cout<<"Enter the number = ";
+        cin>>arr[i];
+    }
+    cout<<"Enter the new number = ";
+    cin>>arr[i];
+
+    for(i=0;i<6+1;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+
+    return 0;
+}
