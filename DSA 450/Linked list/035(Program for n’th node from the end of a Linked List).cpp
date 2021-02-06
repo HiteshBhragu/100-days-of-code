4371d6dca87e96c92d44c69d647c239a54d38053
Hitesh Bhragu <hitesh.bhragu.sdbc@gmail.com>
2021-02-06 10:26:56 +0530

    Linked List

---
+++ DSA 450/Linked list/035(Program for n’th node from the end of a Linked List).cpp
@@ -0,0 +1,86 @@
+// { Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+
+    cout<<getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+/* Should return data of n'th node from the end of linked list.
+*  head: head of the linked list
+*  n: nth node from end to find
+*/
+int getNthFromLast(Node *head, int n)
+{
+       // Your code here
+       Node *head_ref = head;
+       int size = 0;
+       while(head != NULL)
+       {
+           head = head->next;
+           size++;
+       }
+       if(n > size)
+            return -1;
+       n = size-n;
+       head = head_ref;
+
+       for(int i = 0; i < n; i++)
+            head = head->next;
+       return head->data;
+}
+
+
