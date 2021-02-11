2ec0576001d3b783fdb62a81ddee65ac72fe6076
Hitesh Bhragu <hitesh.bhragu.sdbc@gmail.com>
2021-02-11 22:46:06 +0530

    Binary Trees

---
+++ DSA 450/Binary Trees/006(Inorder Traversal of a tree both using recursion).cpp
@@ -0,0 +1,54 @@
+// C program for different tree traversals
+#include <iostream>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+and a pointer to right child */
+struct Node
+{
+	int data;
+	struct Node* left, *right;
+	Node(int data)
+	{
+		this->data = data;
+		left = right = NULL;
+	}
+};
+
+void printInorder(struct Node* node)
+{
+	if (node == NULL)
+		return;
+
+	/* first recur on left child */
+	printInorder(node->left);
+
+	/* then print the data of node */
+	cout << node->data << " ";
+
+	/* now recur on right child */
+	printInorder(node->right);
+}
+
+
+
+/* Driver program to test above functions*/
+int main()
+{
+	struct Node *root = new Node(1);
+	root->left			 = new Node(2);
+	root->right		 = new Node(3);
+	root->left->left	 = new Node(4);
+	root->left->right = new Node(5);
+
+	cout << "\nPreorder traversal of binary tree is \n";
+	printPreorder(root);
+
+	cout << "\nInorder traversal of binary tree is \n";
+	printInorder(root);
+
+	cout << "\nPostorder traversal of binary tree is \n";
+	printPostorder(root);
+
+	return 0;
+}
